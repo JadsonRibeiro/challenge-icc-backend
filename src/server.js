63cdd1f7f115ed847a6b84e5e7311d209df092e6
Jadson Ribeiro (Notebook) <jdsntj@gmail.com>
2021-07-07 23:17:24 -0300

    Edit cors config

--- src/server.js
+++ src/server.js
@@ -14,6 +14,8 @@ const app = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
     app.use(cors());
     next();
 });
